17f27b71fbb1cecc9f7bd3042a663ea8b0ccfbd0
chouflheure <calvignaccharles@gmail.com>
2025-03-03 16:05:24 +0100

    edit viewModel imageStatus

--- CFQ/Widget/Atome/Toogle/CustomToggleStatus.swift
+++ CFQ/Widget/Atome/Toogle/CustomToggleStatus.swift
@@ -34,28 +34,3 @@ struct CustomToggleStatus: View {
         }
     }
 }
-
-import Combine
-import Firebase
-
-class SwitchStatusUserProfileViewModel: ObservableObject {
-    @Published var isActive: Bool = false
-    private var firebase = FirebaseService()
-    private var cancellables = Set<AnyCancellable>()
-    @Published var user: User
-
-    init(user: User) {
-        self.user = user
-
-        user.$isActive
-            .dropFirst() // Ignore la valeur initiale
-            .sink { [weak self] newValue in
-                self?.switchStatusClick(isActive: newValue)
-            }
-            .store(in: &cancellables)
-    }
-
-    private func switchStatusClick(isActive: Bool) {
-        firebase.updateDataByID(data: ["isActive": isActive], to: .users, at: user.uid)
-    }
-}
