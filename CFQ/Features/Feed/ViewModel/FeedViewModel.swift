f618815d996212b9a15907c95bef43a179549433
chouflheure <calvignaccharles@gmail.com>
2025-05-08 03:26:11 +0200

    edit log error

--- CFQ/Features/Feed/ViewModel/FeedViewModel.swift
+++ CFQ/Features/Feed/ViewModel/FeedViewModel.swift
@@ -22,34 +22,10 @@ class FeedViewModel: ObservableObject {
                 switch result {
                 case .success(let turns):
                     self?.turns = turns
-                    print("@@@ sucess = \(turns[0].printObject)")
                 case .failure(let error):
                     print("@@@ here")
-                    print("❌ Erreur : \(error.localizedDescription)")
+                    Logger.log(error.localizedDescription, level: .error)
                 }
             }
     }
-    
-    
-    func catchTurns() {
-        firebaseService.getDataByID(from: .turns, with: "FP5UQvLXVpuSt8N0b9ML") { (result: Result<Turn, Error>) in
-            switch result {
-            case .success(let turn):
-                DispatchQueue.main.async {
-                    self.turns.append(turn)
-                    print(turn.printObject)
-                }
-            case .failure(let e):
-                print("@@@ \(e)")
-            }
-        }
-    }
-        
-    
 }
-
-
-/*
-Turn(uid: "1", titleEvent: "Test", date: nil, pictureURLString: "", admin: "Test", description: "test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test", invited: [""], participants: [""], mood: [0], messagerieUUID: "", placeTitle: "", placeAdresse: "", placeLatitude: 1.2, placeLongitude: 1.2),
-Turn(uid: "2", titleEvent: "Tomoroland", date: nil, pictureURLString: "", admin: "Tomoroland", description: "Tomoroland Tomoroland Tomoroland TomorolandTomorolandTomorolandTomoroland TomorolandTomoroland TomorolandTomoroland TomorolandTomoroland st Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test", invited: [""], participants: [""], mood: [0], messagerieUUID: "", placeTitle: "", placeAdresse: "", placeLatitude: 1.2, placeLongitude: 1.2)]
-*/
